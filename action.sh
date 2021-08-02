4e0e1669bf5351bf201f280921a846987c92c95e
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2021-08-02 13:10:25 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/AvikaTrivedi/phubdl /root/SedLoif
-cp SedLoif/.env /root/SedLoif/.env
+cp purnhubdl-env/.env /root/SedLoif/.env
 cd /root/SedLoif
 docker build . -t fukoff
 docker run --privileged --env-file .env --rm -i fukoff
